7f44ca330231c6f386ed9ec35e5316a18422059b
QA-Rise <qa@riseinteractive.com>
2017-11-06 15:53:30 -0600

    maven-final

---
+++ workspace/RiseTest/src/test/java/RiseSuite/jenkins_demo.java
@@ -0,0 +1,43 @@
+package RiseSuite;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class jenkins_demo {
+
+	@Test
+	public void testgooglesearch() {
+		// System.setProperty("webdriver.gecko.driver","C:\\Users\\Iliona.Iliadhi\\Work
+		// FoldeRS\\Desktop\\geckodriver.exe");
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\users\\iliona.iliadhi\\Work Folders\\Documents\\chromedriver_win32\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+
+		// FirefoxDriver driver = new FirefoxDriver();
+
+		// it will open the goggle page
+
+		driver.get("http://google.in");
+
+		// we expect the title “Google “ should be present
+
+		String Expectedtitle = "Google";
+
+		// it will fetch the actual title
+
+		String Actualtitle = driver.getTitle();
+
+		System.out.println("Before Assetion " + Expectedtitle + Actualtitle);
+
+		// it will compare actual title and expected title
+
+		Assert.assertEquals(Actualtitle, Expectedtitle);
+
+		// print out the result
+
+		System.out.println("After Assertion " + Expectedtitle + Actualtitle + " Title matched ");
+
+	}
+
+}
